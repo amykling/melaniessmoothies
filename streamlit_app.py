e2953394fabc076bbc34db0ceede64881372ad84
amykling <amy.kling@zirous.com>
2024-10-17 14:32:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,11 @@ st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 name_on_order = st.text_input('Name on Smoothie: ')
 st.write("The name on your smoothie will be: ", name_on_order)
 
-
 st.write("Choose the fruits you want in your smoothie!")
 
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -27,17 +25,17 @@ ingredients_list = st.multiselect(
     )
 
 if ingredients_list:
-    st.write(ingredients_list)
-    st.text(ingredients_list)
+   # st.write(ingredients_list)
+   # st.text(ingredients_list)
    
-    ingredients_string=''
+    ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
-    st.write(ingredients_string)
+    #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" +name_on_order+ """')"""
 
